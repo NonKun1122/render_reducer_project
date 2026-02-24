d8b376a34983ef5f6eb6453adf6c664779a21bc2
NonKungCh. <116896756+NonKun1122@users.noreply.github.com>
2026-02-25 06:28:55 +0700

    Add files via upload

---
+++ src/main/java/com/example/mod/RenderReducerModClient.java
@@ -0,0 +1,10 @@
+package com.example.mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RenderReducerModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        System.out.println("Render Reducer Mod Initialized!");
+    }
+}
